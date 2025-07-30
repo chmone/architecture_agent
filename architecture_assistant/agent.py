8057b5cdfd554b9869d6062439051251d1e2851c
chmonesmith <chm24@bu.edu>
2025-07-30 13:29:56 -0400

    cleaning up

--- architecture_assistant/agent.py
+++
@@ -1,7 +0,0 @@
-# Copyright 2025
-# Software Architecture Assistant Agent - Simplified Multi-Agent Pattern
-
-from .agents_simple import root_agent
-
-# Export the root agent for ADK to find
-__all__ = ['root_agent']
